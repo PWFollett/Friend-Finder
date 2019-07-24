32503674a464d3224bc543c7bc6dee12e6b928b7
PWFollett <pwf592110@gmail.com>
2019-07-24 14:26:27 -0400

    working still

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Ted",
+		photo: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2017/05/2017GettyImages-ted-how-i-met-your-mother-band-920x584.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Marshall",
+		photo: "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/9/11/campaign_images/webdr11/marshall-eriksens-best-lines-on-how-i-met-your-mo-2-10552-1447085758-1_dblbig.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Lily",
+		photo: "https://vignette.wikia.nocookie.net/himym/images/5/5e/Lily.jpg/revision/latest?cb=20110809174952",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Robin",
+		photo: "https://static1.purebreak.com/articles/9/72/60/9/@/315203-how-i-met-your-mother-diapo-1.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Barney",
+		photo: "https://vignette.wikia.nocookie.net/himym/images/2/2c/Barney.jpg/revision/latest?cb=20110809174812",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
